f4f23cf8d350518ebc5d7b2f4b24d841e8f1efe0
Aqeel222 <mah222@rocketmail.com>
2016-05-18 20:14:29 +0500

    Make a new method in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def new_method
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
